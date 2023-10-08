5e780ba4aeff97fcb7360db814514229eaf7dea9
Martin <martin.kolberg@outlook.com>
2023-10-08 14:01:33 +0200

    "commiting to pushing all files to ghub"

---
+++ 060923-recept/css_ovning/,-+'+-filer/geo.modernizr.js
@@ -0,0 +1,4 @@
+Window.AkamaiDebug = Window.AkamaiDebug || {};
+Window.AkamaiDebug.remoteAddress = "92.32.98.100";
+Window.AkamaiDebug.remoteAddressCountry = "SE";
+Modernizr.addTest("sweden", function() { return true; });
